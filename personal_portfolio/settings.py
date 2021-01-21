45d0a247dd25f363b569967b1a115bea8f9162ba
Dave Charm <davecharm16@gmail.com>
2021-01-21 11:01:17 +0000

    Remove Old stuff

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'z(ym!#a10cp&@1vdqgw&+dmk1hb=&-wr@^ikulp9u!uh8e#lk*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['davecharm16.pythonanywhere.com']
 
 
 # Application definition
@@ -119,11 +119,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 STATIC_URL = '/static/'
 # STATIC_ROOT =  os.path.join(BASE_DIR, 'static/css')
+STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
